dbf469bd3a523f7762484a3fa0c302cf77431397
='Johnson-ad <=>
2022-06-07 23:55:43 +0000

    Fin de creation du fonction add img in firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
+import { useState, useEffect } from "react"
+import  { storage} from "./data/Data";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { v4} from 'uuid';
+
+
 
 function App() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]); //dispalying all images
+
+  const imageListRef = ref(storage, 'images/');
+  const uploadImage = ()=>{
+    if (imageUpload ==null ) return;
+    const imageRef =ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload ).then((snaphsot)=>{
+      // alert("Image Uploaded");
+      getDownloadURL(snaphsot.ref).then((url)=>{
+        setImageList((prev => [...prev, url]));
+    })
+  })};
+
+  useEffect(()=>{
+    listAll(imageListRef).then(res=>{
+      res.items.forEach(item=>{
+        getDownloadURL(item).then(url=>{
+          setImageList(prev=>[...prev, url])
+        });
+      
+      })
+    })
+  }, []);
+
   return (
     <div className="App">
-       <h1>Hello World</h1>
+      <input type="file" onChange={(e)=>setImageUpload(e.target.files[0])}/>
+      <button onClick={uploadImage}>Upload image</button>
+      <br/>
+
+      {imageList.map(url=>{
+        return <img src={url} alt=""/>
+      })}
     </div>
   );
 }
--- src/data/Data.js
+++ src/data/Data.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
  
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVtnVSC3hpTiv4x9udXi9vAQbcMz9c-wA",
+    authDomain: "upload-images-files.firebaseapp.com",
+    projectId: "upload-images-files",
+    storageBucket: "upload-images-files.appspot.com",
+    messagingSenderId: "184344932417",
+    appId: "1:184344932417:web:64c745edf6662771b596c0",
+    measurementId: "G-K1LYF5FCEC"
+  };
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const storage = getStorage(app);
